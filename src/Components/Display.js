970309c68f73bc2974e8f8c4867521765ab6c5d3
vasanth1066 <vasanthanuvavi@gmail.com>
2024-03-12 23:21:53 +0530

    Building Blocks of Ecommerce

---
+++ src/Components/Display.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Container, Card, Row, Col } from "react-bootstrap";
+
+const Display = () => {
+  const productsArr = [
+    {
+      title: "Colors",
+      price: 100,
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+    },
+
+    {
+      title: "Black and white Colors",
+      price: 50,
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+    },
+
+    {
+      title: "Yellow and Black Colors",
+      price: 70,
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+    },
+
+    {
+      title: "Blue Color",
+      price: 100,
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
+    },
+  ];
+  return (
+    <Container>
+      <h1>hi</h1>
+      <Row>
+        {productsArr.map((item, key) => (
+          <Col key={key} xs={12} md={3} ig={6} className="mb-4">
+            <Card>
+              <Card.Img variant="top" src={item.imageUrl} />
+              <Card.Body>
+                <Card.Title>{item.title}</Card.Title>
+                <Card.Title>{item.price}</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+export default Display;
